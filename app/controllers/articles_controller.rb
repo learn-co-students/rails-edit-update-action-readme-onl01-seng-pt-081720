2630aec70e8c82c893ff677acdced6ad5a0fe5ed
tonitdiep <adiep123@gmail.com>
2021-01-09 02:48:12 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,19 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+  def edit #edit action store the article record in an stance variable
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    #Query the database for the Article record that matches the :id passed to the route.
+    @article.update(title: params[:article][:title], description: params[:article][:desciption])
+    # Store the query in an instance variable.
+    # Update the values passed from the form (the update method here is the update method supplied by Active Record, not the update method we're creating). The update method takes a hash of the attributes for the model as its argument, e.g. `Article.find(1).update(title: "I'm Changed", description: "And here too!")
+    #Save the changes in the database.
+    #Redirect the user to the show page so they can see the updated record.
+    redirect_to article_path(@article)
+    # raise params.inspect    
+  end
 end
